fabaa149ec7adbe9cfd5ed6ad418d2cf45a7421b
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-04-04 11:00:01 -0300

    feat: add handler to find and filter commands

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js')
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js')
+const fs = require('node:fs')
+const path = require('node:path')
 
 const dotenv = require('dotenv')
 dotenv.config()
 const { TOKEN } = process.env
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+client.commands = new Collection()
+
+const commandsPath = path.join(__dirname, 'src/commands')
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter(file => file.endsWith('.js'))
 
 client.once(Events.ClientReady, c => {
   console.log(`Ready! Logged in as ${c.user.tag}`)
