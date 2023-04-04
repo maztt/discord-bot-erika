ea8545b62555ccfc36317438134f7b52cbe986e2
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-04-04 11:03:48 -0300

    feat: add handler to validate and insert commands to the collection

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ const commandFiles = fs
   .readdirSync(commandsPath)
   .filter(file => file.endsWith('.js'))
 
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file)
+  const command = require(filePath)
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command)
+  } else {
+    console.error(`The command at ${filePath} is missing a required "data" or "execute" property.`)
+  }
+}
+
 client.once(Events.ClientReady, c => {
   console.log(`Ready! Logged in as ${c.user.tag}`)
 })
