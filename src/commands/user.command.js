e5c9dd26eb8fb8715a8b31812340cadb04accdf4
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-04-04 10:46:47 -0300

    feat: add /user command

---
+++ src/commands/user.command.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('user')
+    .setDescription('Provides information about the user.'),
+  async execute(interaction) {
+    interaction.reply(
+      `I'm ${interaction.user.username} and I've been here since ${interaction.member.joinedAt}.`
+    )
+  }
+}
