78d1409e44ae7865f03d1c8f06e738312c31a9ec
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-04-06 14:19:04 -0300

    feat: add handler to cooldown command interactions

--- src/events/interaction.event.js
+++ src/events/interaction.event.js
@@ -1,4 +1,7 @@
-const { Events } = require('discord.js')
+const { Client, GatewayIntentBits, Collection, Events } = require('discord.js')
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+client.cooldowns = new Collection()
 
 module.exports = {
   name: Events.InteractionCreate,
@@ -9,6 +12,24 @@ module.exports = {
       console.error(`No command matching ${interaction.commandName} was found.`)
       return
     }
+    const { cooldowns } = client;
+    if (!cooldowns.has(command.data.name)) {
+      cooldowns.set(command.data.name, new Collection())
+    }
+    const now = Date.now()
+    const timestamps = cooldowns.get(command.data.name)
+    const defaultCooldownDuration = 3
+    const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+    if (timestamps.has(interaction.user.id)) {
+      const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+      if (now < expirationTime) {
+        return interaction.reply({ content: `Eita, calma aí! Espera três segundinhos daí tenta o \`${command.data.name}\` de novo.`, ephemeral: true });
+      }
+    }
+    timestamps.set(interaction.user.id, now);
+    setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
     try {
       await command.execute(interaction)
     } catch (error) {
