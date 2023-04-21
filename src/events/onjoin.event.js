a33a6fb2735a0b5a3e0e633c044b77f068223d6d
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-04-21 18:51:32 -0300

    chore: improve welcome message for new users

--- src/events/onjoin.event.js
+++ src/events/onjoin.event.js
@@ -6,7 +6,10 @@ module.exports = {
   async execute(newGuildMember) {
     try {
       const dmChannel = await newGuildMember.createDM()
-      await dmChannel.send(`Que bom te ter aqui, ${newGuildMember.user.username}!`)
+      await dmChannel.send(`
+        Que bom te ter aqui, ${newGuildMember.user.username}! 
+        Antes de mais nada, caso queira fazer um breve onboarding pelo SouJunior, basta me enviar **/onboarding**  reagir no    :roller_coaster:    abaixo!
+      `)
     } catch (err) {
       console.error(
         `An error occurred while trying to send a direct message to ${newGuildMember.user.tag}:`,
